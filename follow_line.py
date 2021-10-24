d835099b59d28ca19594a3e312861dc5b4145e84
Golden Seals <goldensealsfll@gmail.com>
2021-10-24 11:16:43 -0500

    I added definitions for PrimeHub and Timer

--- follow_line.py
+++ follow_line.py
@@ -1,8 +1,11 @@
 # LEGO type:standard slot:3 autostart
 
-from spike import ColorSensor, MotorPair, Motor
+from spike import ColorSensor, MotorPair, Motor, PrimeHub
+from spike.control import Timer
 
+hub = PrimeHub()
 color = ColorSensor('A')
 wheels = MotorPair('F', 'B')
 updown = Motor('E')
 sideside = Motor('C')
+timer = Timer() 
