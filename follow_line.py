99d1199cd84d79b5f0f7275041736ac5c5bd220a
Golden Seals <goldensealsfll@gmail.com>
2021-10-30 14:44:32 -0500

    we added a line following function then we used it for the robot to follow the line.
    we also imported multiplication from the math module

--- follow_line.py
+++ follow_line.py
@@ -2,10 +2,20 @@
 
 from spike import ColorSensor, MotorPair, Motor, PrimeHub
 from spike.control import Timer
+from math import *
 
 hub = PrimeHub()
 color = ColorSensor('A')
 wheels = MotorPair('F', 'B')
 updown = Motor('E')
 sideside = Motor('C')
-timer = Timer() 
+timer = Timer()
+
+def followLine(wheels, color, power):
+    if color.get_color() == 'black':
+        wheels.start_tank( trunc(power * 0 / 3), power)
+    else:
+        wheels.start_tank(power, trunc(power * 0 / 3))
+    
+while True:
+    followLine(wheels, color, 40)
