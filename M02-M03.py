87593c3e4110041f0afa4419089fbddd7f8351a9
Golden Seals <goldensealsfll@gmail.com>
2021-11-06 16:56:52 -0500

    we made the robot move forward, push down the wall for the cargo plane, move backwards, move forwards, and push the rest of it down.

--- M02-M03.py
+++ M02-M03.py
@@ -1,18 +1,30 @@
 # LEGO type:standard slot:0 autostart
-
+# left wall 4 1/2 squares from wood
 from spike import ColorSensor, MotorPair, Motor, PrimeHub
 from spike.control import Timer
-from math import *
+import math
 
 hub = PrimeHub()
 color = ColorSensor('A')
 wheels = MotorPair('F', 'B')
-updown = Motor('E')
-sideside = Motor('C')
+updown = Motor('C')
+sideside = Motor('E')
 timer = Timer()
 
 # move foward
-# use the bottom attatchment to move the cargo package into the home base
-# move foward
+wheels.move(11 * math.pi / 2, 'in', steering=0, speed=30)
+
 # use top attatchment to bring down the cargo in the plane
-# turn & go straight int the home base
+updown.run_for_rotations(0.75)
+
+# move back 
+wheels.move(-1 * math.pi / 2, 'in', steering=0, speed=50)
+
+# move wall up
+updown.run_for_rotations(-0.75)
+
+# move forward
+wheels.move(0.5 * math.pi / 2, 'in', steering=0, speed=50)
+
+# move wall down
+updown.run_for_rotations(0.65, speed= 100)
