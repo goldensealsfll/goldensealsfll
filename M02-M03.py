ecba5923cdffe90d4c02b79bdeb8b8777a523570
Golden Seals <goldensealsfll@gmail.com>
2021-11-13 15:39:14 -0600

    made progress on missions 2 & 3 (unloading plane & filling the unused capacity.)

--- M02-M03.py
+++ M02-M03.py
@@ -1,7 +1,7 @@
 # LEGO type:standard slot:0 autostart
 # left wall 4 1/2 squares from wood
 from spike import ColorSensor, MotorPair, Motor, PrimeHub
-from spike.control import Timer
+from spike.control import Timer, wait_for_seconds
 import math
 
 hub = PrimeHub()
@@ -10,21 +10,31 @@ wheels = MotorPair('F', 'B')
 updown = Motor('C')
 sideside = Motor('E')
 timer = Timer()
+wheels.set_motor_rotation(8.8 * math.pi, 'cm')
 
 # move foward
-wheels.move(11 * math.pi / 2, 'in', steering=0, speed=30)
+wheels.move(25.5, 'in', steering=0, speed=30)
 
 # use top attatchment to bring down the cargo in the plane
-updown.run_for_rotations(0.75)
+updown.run_for_rotations(0.65)
 
-# move back 
-wheels.move(-1 * math.pi / 2, 'in', steering=0, speed=50)
+# move back
+wheels.move(-1.9, 'in', steering=0, speed=50)
 
 # move wall up
 updown.run_for_rotations(-0.75)
 
+# move top attachment to the side
+sideside.run_for_rotations(-0.50)
+
 # move forward
-wheels.move(0.5 * math.pi / 2, 'in', steering=0, speed=50)
+wheels.move(1, 'in', steering=0, speed=50)
 
 # move wall down
 updown.run_for_rotations(0.65, speed= 100)
+
+# move forward
+wheels.move(1.5, 'in', steering=0, speed=50)
+
+# return to home and take toolbox with
+wheels.move(-25, 'in', steering=0, speed=50)
