8158f3aa0accc2a57b14e6c6ff8954382ecb96e7
Nicholas Tuzhilov <nicholas.tuzhilov@gmail.com>
2022-01-17 14:23:02 -0600

    we made the right correct changes

--- M07.py
+++ M07.py
@@ -6,74 +6,73 @@ import math
 
 hub = PrimeHub()
 wheels = MotorPair('F', 'B')
-wheel1= Motor('B')
 updown = Motor('C')
 sideside = Motor('E')
 timer = Timer
-wheels.set_motor_rotation(8.8 * math.pi, 'cm')
 
 # move foward
-wheels.move(13, 'in', steering=0, speed=30)
+wheels.move(9, 'in', steering=0, speed=30)
 
 # turn to hit car 
-wheels.move(184, 'degrees', steering=107, speed=20)
+wheels.move(178, 'degrees', steering=100, speed=20)
 
 # move foward to hit car
-wheels.move(24, 'in', steering=0, speed=40)
+wheels.move(15, 'in', steering=0, speed=40)
 
 # move back 
-wheels.move(-4.5, 'in', steering=0, speed=20)
+wheels.move(-3, 'in', steering=0, speed=20)
 
 # turn to face the thing
 wheels.move(-137,'degrees', steering=79, speed=25)
 
 # go to the thing
-wheels.move(18.5, 'in', steering=0, speed=30)
+wheels.move(15, 'in', steering=0, speed=25)
 
 # turn to the thing
-wheels.move(130, 'degrees', steering=75, speed=30)
+wheels.move(120, 'degrees', steering=73, speed=25)
 
 # move foward
-wheels.move(8, 'in', steering=0, speed=30)
+wheels.move(4.5, 'in', steering=0, speed=30)
 
 #move back
-wheels.move(-10, 'in', steering=0, speed=35)
+wheels.move(-7, 'in', steering=0, speed=35)
 
 # turn
-wheels.move(195, 'degrees', steering=50, speed=35)
+wheels.move(185, 'degrees', steering=53, speed=35)
 
 # Move forward
 wheels.move(10, 'in', steering=0, speed=35)
 
-# Turn left
-wheels.move(150, 'degrees', steering=-90, speed=35)
+# Turn left to helicopter
+wheels.move(155, 'degrees', steering=-115, speed=35)
 
 #Move forward
-wheels.move(25, 'in', steering=0, speed=35)
-
+wheels.move(15, 'in', steering=0, speed=35)
+raise SystemExit
 # move back
-wheels.move(-7, 'in', steering=0, speed=20)
+wheels.move(-6, 'in', steering=0, speed=20)
 
 # turn
-wheels.move(230, 'degrees', steering=180, speed=20)
+wheels.move(250, 'degrees', steering=205, speed=20)
 
 # move foward
-wheels.move(6, 'in', steering=0, speed=20)
+wheels.move(3, 'in', steering=0, speed=20)
 
 # turn again
-wheels.move(200, 'degrees', steering=150, speed=20)
+wheels.move(210, 'degrees', steering=156, speed=20)
 
 # move foward
-wheels.move(15, 'in', steering=0, speed=30)
+wheels.move(13, 'in', steering=0, speed=30)
 
 # move wall up
 updown.run_for_rotations(-100, speed=80)
 
 # move foward
-wheels.move(19, 'in', steering=0, speed=30)
+wheels.move(17, 'in', steering=0, speed=30)
 
 # turn
 wheels.move(-140, 'degrees', steering=75, speed=20)
 
 # move foward 
 wheels.move(35, 'in', steering=0, speed=30)
+
