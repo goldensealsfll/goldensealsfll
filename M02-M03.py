cbc0adf42039809a1b8aaaaca77a5ef8f2807ce9
Golden Seals <goldensealsfll@gmail.com>
2022-01-27 19:13:26 -0600

    We made a code to do the cargo plane and the unused capacity. We also used a shovel- like attachment to complete the missions.

--- M02-M03.py
+++ M02-M03.py
@@ -1,5 +1,5 @@
 # LEGO type:standard slot:0 autostart
-# left wall 4 1/2 squares from wood
+# left wall 5.5 squares from wood
 from spike import ColorSensor, MotorPair, Motor, PrimeHub
 from spike.control import Timer, wait_for_seconds
 import math
@@ -12,7 +12,10 @@ timer = Timer()
 wheels.set_motor_rotation(8.8 * math.pi, 'cm')
 
 # move foward
-wheels.move(25.7, 'in', steering=0, speed=30)
+wheels.move(24.7, 'in', steering=0, speed=30)
+
+#turn to face cargo plane
+wheels.move(137,'degrees', steering=-79, speed=25)
 
 # use top attatchment to bring down the cargo in the plane
 updown.run_for_rotations(0.75)
@@ -24,7 +27,7 @@ wheels.move(-1.9, 'in', steering=0, speed=50)
 updown.run_for_rotations(-0.90)
 
 # move forward
-wheels.move(1.9, 'in', steering=0, speed=50)
+wheels.move(2, 'in', steering=0, speed=50)
 
 # move wall down
 updown.run_for_rotations(0.75, speed= 100)
@@ -35,6 +38,18 @@ while (count < 3):
     count = count + 1
     wheels.move(2, 'in', steering=0, speed=75)
     wheels.move(-2, 'in', steering=0, speed=75)
+
+# move forward
+wheels.move(2, 'in', steering=0, speed=50)
+
+# move wall down
+updown.run_for_rotations(0.75, speed= 100)
+
+#turn back
+wheels.move(-2.2, 'in', steering=0, speed=35)
+
+#turn back
+wheels.move(137,'degrees', steering=85, speed=25)
 
 # return to home and take toolbox with
 wheels.move(-24, 'in', steering=0, speed=50)
