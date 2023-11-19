0ddd2c0eba13ed3c1a8f4467677905317fd166f3
Golden Seals <goldensealsfll@gmail.com>
2023-11-19 15:58:11 -0800

    Coded our robot to go to home base!

--- Run2.py
+++ Run2.py
@@ -19,59 +19,68 @@ while True:
 
     if selected == "B":
         #Getting in position by turning and going straight  to move forward to concert stage
-        #drive_base.straight(160)
-        #drive_base.turn(-90)
-        #drive_base.straight(230)
-        #drive_base.turn(90)
+        drive_base.straight(160)
+        drive_base.turn(-90)
+        drive_base.straight(230)
+        drive_base.turn(90)
 
         #move forward to music concert, turning, and pushing in concert stage
-        #drive_base.straight(540)
-        #drive_base.turn(45)
-        #drive_base.straight(135)
+        drive_base.straight(540)
+        drive_base.turn(45)
+        drive_base.straight(135)
 
         #Moving back from pushing in mission
-        #drive_base.straight(-135)
+        drive_base.straight(-135)
         
         #Turning to go forward
-        #drive_base.turn(45)
+        drive_base.turn(45)
         
         #Going forward to pull back lever, turning, and pulling back lever of lights
-        #drive_base.straight(225)
-        #drive_base.turn(-45)
-        #arm_motor.run_angle(600,-125)
+        drive_base.straight(210)
+        drive_base.turn(-45)
+        arm_motor.run_angle(600,-135)
         
         #Turning to move backwards and away from music concert
-        #drive_base.turn(-30)
-        #drive_base.straight(-450)
+        drive_base.turn(-30)
+        drive_base.straight(-450)
 
         #Turning, going forward to push in craft creator
-        #drive_base.turn(-60)
-        #drive_base.straight(350)
+        drive_base.turn(-60)
+        drive_base.straight(345)
         
         #Using arm to lift back craft creator lid
-        #arm_motor.run_angle(100,-85)
-        #wait(10)
-        #arm_motor.run_angle(100, 15)
+        arm_motor.run_angle(100,-80)
+        wait(10)
+        arm_motor.run_angle(100, 15)
         
         #Turning and moving backwards to get onto position to move to the stage
-        #drive_base.turn(-10)
-        #drive_base.straight(-200)
+        drive_base.turn(-10)
+        drive_base.straight(-200)
 
-        #Turning and moving to statue
-        drive_base.turn(20)
+        #Turning and moving to statue (moving hand up)
+        drive_base.turn(23)
         drive_base.straight(400)
+        arm_motor.run_angle(200,200)
+
         
         #Turning to statue and getting in position for statue
-        drive_base.turn(-30)
+        drive_base.turn(-37)
         drive_base.straight(400)
         drive_base.turn(120)
-        drive_base.straight(50)
-        arm_motor.run_angle(200,-200)
-        drive_base.turn(80)
-        drive_base.straight(150)
+        drive_base.straight(55)
+        arm_motor.run_angle(200,-215)
+        drive_base.turn(75)
+        drive_base.straight(175)
 
+        #Heading back to home base
+        drive_base.turn(45)
+        drive_base.straight(100)
+        arm_motor.run_angle(200,200)
+        drive_base.turn(-70)
+        drive_base.straight(900)
+ 
 
-
+        
 
 
     
