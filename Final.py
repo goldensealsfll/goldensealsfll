4672b0c3106a186b832bd3db0d57ac0fd8e9672a
Golden Seals <goldensealsfll@gmail.com>
2023-12-17 15:49:11 -0600

    we updated for new firmware version

--- Final.py
+++ Final.py
@@ -1,15 +1,16 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorDistanceSensor
 from pybricks.parameters import Axis, Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import GyroDriveBase
+from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, hub_menu
 
 hub = PrimeHub(top_side=Axis.Z, front_side=-Axis.Y)
 left_wheel = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 right_wheel = Motor(Port.E,Direction.CLOCKWISE)
 arm_motor = Motor(Port.C)
-drive_base = GyroDriveBase (left_wheel, right_wheel, 88, 112)
+drive_base = DriveBase (left_wheel, right_wheel, 88, 112)
 
+drive_base.use_gyro(True)
 drive_base.settings(straight_speed=300, turn_rate=100) 
 
 while True:
